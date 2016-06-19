a94b16b9b40eed1cdd664af0c985ef6429af2ff1
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-06-19 19:21:33 +1000

    Added dotenv & installed new dependencies

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
-const port = process.env.PORT || 4000
+dotenv.config();
+const port = process.env.PORT || 3000
 
 app.get('*', function(req, res) {
   res.json({'Initial Message': 'Welcome to NodeJs User Crud Backend fro SPAs.'});
